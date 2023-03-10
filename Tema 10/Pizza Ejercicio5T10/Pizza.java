995bdbd56976d652029fdab7d9e96f94a9480105
MarioNadales <mnadsua290@g.educaand.es>
2023-03-10 19:43:46 +0100

    MNS

--- Tema 10/Pizza Ejercicio5T10/Pizza.java
+++ Tema 10/Pizza Ejercicio5T10/Pizza.java
@@ -2,17 +2,17 @@ public class Pizza {
   
   private static int totalPedidas = 0;
   private static int totalServidas = 0;
-  private String tamano;
+  private String tamaño;
   private String tipo;
   private String estado;
-  public Pizza(String tipo, String tamano) {
+  public Pizza(String tipo, String tamaño) {
   this.tipo = tipo;
-  this.tamano = tamano;
+  this.tamaño = tamaño;
   this.estado = "pedida";
   Pizza.totalPedidas++;
   }
   public String toString() {
-  return "pizza " + this.tipo + " " + this.tamano + ", " + this.estado;
+  return "pizza " + this.tipo + " " + this.tamaño + ", " + this.estado;
   }
   public static int getTotalPedidas() {
   return Pizza.totalPedidas;
