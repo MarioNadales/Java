75076b9c803877f7de5b5dae4deb69459423efb4
MarioNadales <mnadsua290@g.educaand.es>
2023-03-14 19:02:13 +0100

    MNS

--- Tema 10/Pizza Ejercicio5T10/PedidosPizas.java
+++ Tema 10/Pizza Ejercicio5T10/PedidosPizas.java
@@ -6,9 +6,12 @@ public class PedidosPizas {
   Pizza p2 = new Pizza("funghi", "familiar");
   p2.sirve();
   Pizza p3 = new Pizza("cuatro quesos", "mediana");
+  Pizza p4 = new Pizza("Prosiuto", "grande");
+  p4.sirve();
   System.out.println(p1);
   System.out.println(p2);
   System.out.println(p3);
+  System.out.println(p4);
   p2.sirve();
   System.out.println("pedidas: " + Pizza.getTotalPedidas());
   System.out.println("servidas: " + Pizza.getTotalServidas());
